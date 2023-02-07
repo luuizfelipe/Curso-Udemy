8fb31c38a4bc5ec35ecd4d78798f1432f091282a
luuizfelipe <luiz.nascimentodev96@gmail.com>
2023-02-06 21:53:06 -0300

    teste1

---
+++ Var/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "maria";
+
+            Console.WriteLine(x + y + z);
+        }
+    }
+}
